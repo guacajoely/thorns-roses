87663e57411eafa8582d880821c82736b66dc702
guacajoely <guacajoely@gmail.com>
2023-04-18 11:27:45 -0400

    flower list working

--- src/components/nurseries/nursery.js
+++ src/components/nurseries/nursery.js
@@ -2,7 +2,13 @@ export const Nursery = ({ id, name, flowers, distributors }) => {
 
     return <section className="nursery" key={`nursery--${id}`}>
                 <header>{name}</header>
+                <ul>Flowers</ul>
+                {
+                    flowers.map(flower => {
+                    return <li> {flower.color} {flower.species}s ${flower.price.toFixed(2)}</li>
+                })
                 
+                }
             </section>
     
 }
--- src/components/nurseries/nurseryList.js
+++ src/components/nurseries/nurseryList.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
-import { getNurseries } from "../ApiManager.js"
+import { getNurseries, getNurseryStock } from "../ApiManager.js"
 import { Nursery } from "./nursery.js"
 
 export const NurseryList = () => {
 
     const [nurseries, setNurseries] = useState([])
+    const [nurseryStock, setNurseryStock] = useState([])
+    const [distributors, setDistributors] = useState([])
 
     useEffect(
         () => {
@@ -16,15 +18,45 @@ export const NurseryList = () => {
         [] // When this array is empty, you are observing initial component state
     )
 
+    useEffect(
+        () => {
+            getNurseryStock()
+            .then((responseArray) => {
+                setNurseryStock(responseArray)
+            })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+   
+
+    console.log(nurseryStock)
+
     return <>
     <h2>List of Nurseries</h2>
     <article className="nurseries">
 
-    {nurseries.map(nursery => <Nursery id={nursery.id} 
-                                        name={nursery.name} 
-                                        />)
+    {
+        
+        nurseries.map(nursery => {
 
-            }
+            const flowersArray = nurseryStock.map(object => {
+                if(object.nurseryId === nursery.id){
+                    return {
+                        color : object.flower.color,
+                        species: object.flower.species,
+                        price : object.price
+                    }
+                }
+            }).filter(x => {return x !== undefined})
+
+            
+            console.log(flowersArray)
+
+            return <Nursery id={nursery.id} name={nursery.name} flowers={flowersArray}/>
+
+                })
+    }
 
     </article>
     </>
