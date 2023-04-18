a9728d6acf25fbd254f060fd22e0083e916a5682
guacajoely <guacajoely@gmail.com>
2023-04-18 11:57:07 -0400

    ch 3 complete

--- src/components/nurseries/nursery.js
+++ src/components/nurseries/nursery.js
@@ -3,12 +3,14 @@ export const Nursery = ({ id, name, flowers, distributors }) => {
     return <section className="nursery" key={`nursery--${id}`}>
                 <header>{name}</header>
                 <ul>Flowers</ul>
-                {
-                    flowers.map(flower => {
-                    return <li> {flower.color} {flower.species}s ${flower.price.toFixed(2)}</li>
-                })
-                
-                }
+                {flowers.map(flower => {
+                    return <li className="flower" key={flower.id}> {flower.color} {flower.species}s ${flower.price.toFixed(2)}</li>
+                })}
+
+                <ul>Distributors</ul>
+                {distributors.map(distributor => {
+                    return <li className="distributor" key={distributors.indexOf(distributor)}> {distributor}</li>
+                })}
             </section>
-    
+            
 }
--- src/components/nurseries/nurseryList.js
+++ src/components/nurseries/nurseryList.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react"
-import { getNurseries, getNurseryStock } from "../ApiManager.js"
+import { getDistributorStock, getNurseries, getNurseryStock } from "../ApiManager.js"
 import { Nursery } from "./nursery.js"
 
 export const NurseryList = () => {
 
     const [nurseries, setNurseries] = useState([])
     const [nurseryStock, setNurseryStock] = useState([])
-    const [distributors, setDistributors] = useState([])
+    const [distributorStock, setDistributorStock] = useState([])
 
     useEffect(
         () => {
@@ -28,6 +28,16 @@ export const NurseryList = () => {
         [] // When this array is empty, you are observing initial component state
     )
 
+    useEffect(
+        () => {
+            getDistributorStock()
+            .then((responseArray) => {
+                setDistributorStock(responseArray)
+            })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
    
 
     console.log(nurseryStock)
@@ -40,20 +50,39 @@ export const NurseryList = () => {
         
         nurseries.map(nursery => {
 
+            //LOOP THROUGH NURSERYSTOCK TO GRAB FLOWERS
             const flowersArray = nurseryStock.map(object => {
+                //ONLY GRAB FLOWERS THAT MATCH NURSERY ID
                 if(object.nurseryId === nursery.id){
+                    //MAKE A NEW "FLOWER" OBJECT THAT WE'RE GOING TO PASS TO NURSERY.JS
                     return {
+                        id: object.id,
                         color : object.flower.color,
                         species: object.flower.species,
                         price : object.price
                     }
                 }
+                //GET RID OF UNDEFINED RETURNS
             }).filter(x => {return x !== undefined})
 
-            
-            console.log(flowersArray)
+            //LOOP THROUGH DISTRIBUTORSTOCK TO GRAB NAMES OF ALL DISTRIBUTORS USING THAT NURSERY
+            const distributorsArray = distributorStock.map(object => {
+                //ONLY GRAB DISTRIBUTORS THAT MATCH NURSERY ID
+                if(object.nurseryId === nursery.id){
+                    //RETURN THE NAME OF EACH DISTRIBUTOR
+                    return object.distributor.name
+                }
+                //GET RID OF UNDEFINED RETURNS
+            }).filter(x => {return x !== undefined})
 
-            return <Nursery id={nursery.id} name={nursery.name} flowers={flowersArray}/>
+            console.log(distributorsArray)
+
+            //BUILD HTML FOR EACH NURSERY IN NURSERIES   
+            return <Nursery id={nursery.id} 
+                            name={nursery.name} 
+                            flowers={flowersArray}
+                            distributors={distributorsArray}
+            />
 
                 })
     }
