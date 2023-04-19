d5e7d5aa76e830adee0dfde1ce73e9d81d057e6b
guacajoely <guacajoely@gmail.com>
2023-04-19 11:06:41 -0400

    distributor list working in retailer component

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -3,6 +3,16 @@ export const getNurseries = () => {
     .then(response => response.json())
 }
 
+export const getDistributors = () => {
+    return fetch('http://localhost:8088/distributors?_embed=distributorStock&_embed=retailerStock')
+    .then(response => response.json())
+}
+
+export const getRetailers = () => {
+    return fetch('http://localhost:8088/retailers?_embed=retailerStock')
+    .then(response => response.json())
+}
+
 export const getNurseryStock = () => {
     return fetch('http://localhost:8088/nurseryStock?_expand=flower&_expand=nursery')
     .then(response => response.json())
@@ -13,17 +23,7 @@ export const getDistributorStock = () => {
     .then(response => response.json())
 }
 
-export const getDistributors = () => {
-    return fetch('http://localhost:8088/distributors?_embed=distributorStock&_embed=retailerStock')
-    .then(response => response.json())
-}
-
 export const getRetailerStock = () => {
     return fetch('http://localhost:8088/retailerStock?_expand=distributor&_expand=retailer')
-    .then(response => response.json())
-}
-
-export const getRetailers = () => {
-    return fetch('http://localhost:8088/retailers?_embed=retailerStock')
     .then(response => response.json())
 }
