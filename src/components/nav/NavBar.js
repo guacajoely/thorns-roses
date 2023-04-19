3192313a65ca1af84f9b86d2807d540cb77d3c2b
guacajoely <guacajoely@gmail.com>
2023-04-19 10:26:21 -0400

    add home link

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,6 +6,9 @@ export const NavBar = () => {
     return (
         <ul className="navbar">
             <li className="navbar__item active">
+                <Link className="navbar__link" to="/">Home</Link>
+            </li>
+            <li className="navbar__item active">
                 <Link className="navbar__link" to="/nurseries">Nurseries</Link>
             </li>
             <li className="navbar__item active">
