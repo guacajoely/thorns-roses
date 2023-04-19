9fd921e1bfa95db2ed2f370a31fef5882e7f78c5
guacajoely <guacajoely@gmail.com>
2023-04-19 14:13:22 -0400

    login/register working

---
+++ src/components/auth/login.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import "./login.css"
+
+export const Login = () => {
+    const [email, set] = useState("joel@joel.com")
+    const navigate = useNavigate()
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        return fetch(`http://localhost:8088/customers?email=${email}`)
+            .then(res => res.json())
+            .then(foundUsers => {
+                if (foundUsers.length === 1) {
+                    const user = foundUsers[0]
+                    localStorage.setItem("current_user", JSON.stringify({
+                        id: user.id
+                    }))
+
+                    navigate("/")
+                }
+                else {
+                    window.alert("Invalid login")
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>Thorns and Roses</h1>
+                    <h2>Please sign in</h2>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input type="email"
+                            value={email}
+                            onChange={evt => set(evt.target.value)}
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit">
+                            Sign in
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+            </section>
+        </main>
+    )
+}
